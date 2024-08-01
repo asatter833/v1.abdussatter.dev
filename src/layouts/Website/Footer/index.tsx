09fde21b1fb55fa8af719fea47189ab07ca9a3a2
abdus satter <asatter833@gmail.com>
2024-08-01 14:59:57 +0600

    footer changed

--- src/layouts/Website/Footer/index.tsx
+++ src/layouts/Website/Footer/index.tsx
@@ -1,25 +1,33 @@
 import React from "react";
 import { Icon } from "@iconify/react";
+import data from "../../../pages/Home/components/Hero/data.json";
+import { Link } from "react-router-dom";
 
 const WebsiteFooter: React.FC = () => {
-	return (
-		<footer className="bg-[#020000] border-t-primary-900 border-opacity-65 border-t mt-7 py-7">
-			<div className="flex flex-col sm:flex-row items-center justify-between max-w-6xl px-4 mx-auto">
-				<div className="flex items-center gap-2 justify-center text-xs font-semibold tracking-widest text-slate-400">
-					Designed by{" "}
-					<Icon
-						icon="pixelarticons:heart"
-						className="text-primary"
-					/>{" "}
-					<span className="text-white">TAM</span>
-				</div>
-				<p className="text-xs font-semibold tracking-widest text-slate-400">
-					tam11a.dev &copy;{" "}
-					<span className="text-primary">{new Date().getFullYear()}</span>
-				</p>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="bg-[#020000] border-t-primary-900 border-opacity-65 border-t mt-7 py-6">
+      <div className="flex flex-col sm:flex-row items-center justify-between max-w-5xl px-4 mx-auto gap-1">
+        <div className="flex items-center gap-2 justify-center tracking-wides">
+          {data.socials?.map((social, index) => (
+            <React.Fragment key={index}>
+              <Link
+                to={social.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex flex-row items-center gap-1 text-primary hover:text-primary-500 transition-colors duration-300 tracking-widest"
+              >
+                <Icon icon={social.icon} className="text-2xl" />
+              </Link>
+            </React.Fragment>
+          ))}
+        </div>
+        <p className="text-xs font-medium tracking-widest text-gray-400">
+          Copyright &copy; Abdus Satter <span>{new Date().getFullYear()}</span>{" "}
+          All rights Reserved{" "}
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default WebsiteFooter;
--- src/layouts/Website/index.tsx
+++ src/layouts/Website/index.tsx
@@ -5,17 +5,17 @@ const WebsiteHeader = lazy(() => import("./Header"));
 const WebsiteFooter = lazy(() => import("./Footer"));
 
 const WebsiteLayout: React.FC = () => {
-	return (
-		<>
-			<section className="bg-[url(/grid.svg)] bg-repeat-y bg-cover">
-				<WebsiteHeader />
-				<main className="max-w-6xl mx-auto min-h-[95vh]">
-					<Outlet />
-				</main>
-				<WebsiteFooter />
-			</section>
-		</>
-	);
+  return (
+    <>
+      <section className="bg-[url(/grid.svg)] bg-repeat-y bg-cover">
+        <WebsiteHeader />
+        <main className="max-w-6xl mx-auto min-h-[95vh]">
+          <Outlet />
+        </main>
+        <WebsiteFooter />
+      </section>
+    </>
+  );
 };
 
 export default WebsiteLayout;
